32d707e71544a9829da8beb5cdeb8610aa52fd4e
nidhiarora6262 <nidhiarora6262@gmail.com>
2020-01-21 16:54:19 +0530

    update

--- Task2/src/main/scala/Demo/Timestamp.scala
+++ Task2/src/main/scala/Demo/Timestamp.scala
@@ -5,7 +5,7 @@ import org.apache.spark
 import org.apache.spark.sql.{Column, DataFrame, SparkSession, functions}
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{lead, _}
-class Window {
+class Windows {
 
 
   val spark = SparkSession.builder().master("local[*]").appName("example of SparkSession")
@@ -13,54 +13,77 @@ class Window {
     .getOrCreate()
 
    //read the data
-  val words = spark.read.option("header", "true").csv("/home/nidhi/Documents/input.csv")
+  val words = spark.read.option("header", "true").csv("/home/nidhi/Desktop/sample_input.csv")
   words.show()
 
   def range(DEVICE_ID: Int, TIMESTAMP: String, FLOWRATE: Double): DataFrame = {
 
-    val window = Window.orderBy("DEVICE_ID")
-    // previous and next columns of flow rate
+    val window = Window.partitionBy("DEVICE_ID").orderBy("TIMESTAMP")
+
     val df = words.withColumn("previous", functions.lag("FLOW_RATE", 1).over(window)).
-      withColumn("next", functions.lead("FLOW_RATE", 1).over(window))
-    df.show()
-    //apply conditions
-    val df2 = df.withColumn("start_flowrate", functions.when(functions.col("previous") > 0.0D and functions.col("next") >= 0.0D, 0.0D)).
-      withColumn("end_flowrate", functions.when(functions.col("next") equalTo 0.0D, 0.0D))
-    df2.show()
-     //find when meet conditions then previous start flow rate
+      withColumn("next", functions.lead("FLOW_RATE", 0).over(window))
+    df.show(30)
+
+    //df.filter("previous == 0 and next == 0 and FLOW_RATE ==0" ).drop().show()
+
+    ////val df8=df.select("DEVICE_ID","FLOW_RATE","TIMESTAMP","PRESSURE","MODE","VALVE_STATE","previous","next").where(" FLOW_RATE !=0.0D and  previous != 0.0D and next != 0.0D")
+    //val df8=df.filter(" COALESCE(previous and  next and FLOW_RATE) IS NOT NULL")
+    //df8.show()
+    //df.filter("df.previous isNull and df.next isNull  and df.FLOW_RATE isNull").drop().show()
+    //df.drop((col("previous") == 0 && col("next")  == 0&& col("FLOW_RATE") ==0)).show()
+
+    // df.select("next").show()
+
+    val df2 = df.withColumn("start_flowrate", functions.when(functions.col("previous") > 0.0D and functions.col("next") >= 0.0D, 0).otherwise(1.0))
+      .withColumn("end_flowrate", functions.when(functions.col("next") equalTo 0.0D and functions.col("previous") > 0.0D, 0).otherwise(1.0))
+    df2.show(30)
+
+
     val df3 = df2.withColumn("previousstartflowrate", functions.lag("start_flowrate", 1).over(window))
-    df3.show()
-     //for getting the first value of group apply condition
-    val df4 = df3.withColumn("starttimestamp", functions.when(functions.col("previous") > 0.0D and functions.col("previousstartflowrate") > 0.0D, 1))
-    df4.show()
-    //fill that with timestamp
-    val df5 = df4.withColumn("starttimestamp1", when(df4.col("starttimestamp") isNull, value = 0).otherwise(df2.col("TIMESTAMP")))
-    df5.show()
-    //find when meet conditions then previous start flow rate
-    val df6 = df5.withColumn("previousendflowrate", functions.lag("end_flowrate", 1).over(window))
-    df6.show()
+    df3.show(30)
 
-    val df7 = df6.withColumn("endtimestamp", functions.when(functions.col("end_flowrate") equalTo 0.0D and functions.col("previousendflowrate") > 0.0D, 1))
-    df7.show()
-    //fill that with timestamp
-    val df8 = df7.withColumn("endtimestamp1", when(df7.col("endtimestamp") isNull, 0).otherwise(df2.col("TIMESTAMP")))
+    val df4 = df3.withColumn("starttimestamp", functions.when(functions.col("previous") > 0.0D and functions.col("previousstartflowrate") > 0.0D,df2.col("TIMESTAMP")))
+    df4.show(30)
+
+
+    val df6 = df4.withColumn("previousendflowrate", functions.lag("end_flowrate", 1).over(window))
+    df6.show(30)
+    val df7 = df6.withColumn("endtimestamp", functions.when(functions.col("end_flowrate") equalTo 0.0D and functions.col("previousendflowrate") > 0.0D,df2.col("TIMESTAMP")))
+    df7.show(200)
+    //df10.withColumn("finaltimestamp2",functions.col(df10.select("startTimestamp").distinct(). as ("starttimestamp1"))).show()
+    //df10.withColumn("finaltimestamp1",functions.col(df10.select("starttimestamp").distinct()
+
+    //df10.withColumn("finaltimestamp2",functions.col(df10.select("startTimestamp").distinct(). as ("starttimestamp1"))).show()
+    val window2 = Window.partitionBy("DEVICE_ID").orderBy("TIMESTAMP")
+    //df10.withColumn("finaltimestamp2",functions.col(df10.select("startTimestamp").distinct(). as ("starttimestamp1"))).show()
+    val df8=df7.withColumn("finaltimestamp1",first("starttimestamp", true).over(window)).
+      withColumn("finaltimestamp2",last("endtimestamp", true).over(window2))
+
     df8.show()
-    //final timestamp
-    df5.select("starttimestamp1").distinct.as("Finalstarttimestamp").show()
-    df8.select("endtimestamp1").distinct.as("Finalendtimestamp").show()
 
-    df=window.partitionBy("DEVICE_ID").orderBy("DEVICE_ID")
+    df8.groupBy( "finaltimestamp1","finaltimestamp2" ).count().show()
+
+
+
+
+      //fill that with timestamp
+
+    //final timestamp
+   // df5.select("starttimestamp1").distinct.as("Finalstarttimestamp").show()
+    //df8.select("endtimestamp1").distinct.as("Finalendtimestamp").show()
+
+    //df=window.partitionBy("DEVICE_ID").orderBy("DEVICE_ID")
 
     //for month column
     df.select(date_format(col("TIMESTAMP"), "yyyy-MM-dd").alias("Month").cast("date"))
     df
   }
 }
-    object Timestamp  extends Window {
+    object Timestamp  extends Windows {
    def main(args:Array[String]) :Unit={
 
 
-     val a = new Window()
+     val a = new Windows()
      a.range(1, "2019,23,2", 0.5)
 
 
@@ -69,7 +92,8 @@ class Window {
   }
 
 
-
+//val df8 = df7.withColumn("endtimestamp1", when(df7.col("endtimestamp") isNull, 0).otherwise(df2.col("TIMESTAMP")))
+//df8.show()
 
 //df2.select("start_flow").show()
 /* val df3 = df2.withColumn("timestamp1",
@@ -85,7 +109,9 @@ class Window {
  df4.select("timestamp5").show()
  df4
 */
-
+//fill that with timestamp
+//val df5 = df4.withColumn("starttimestamp1", when(df4.col("starttimestamp") isNull, value = 0).otherwise(df2.col("TIMESTAMP")))
+//df5.show()
 
 // val df5 = functions.when(df4.col("timestamp1") isNull, df4.select(first("timestamp5")).as("newtimestamp"))
 
